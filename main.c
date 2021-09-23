8889a9aa5b4a6a219e719bad1ff5b838951b9f1a
youssengaber <91127735+youssengaber@users.noreply.github.com>
2021-09-23 15:46:46 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int x = 5;
+  int y = 7;
+  float PI = 3.14;
+  float LS = 299792458;  
+  
+  printf("I have %d apples, while john has %d apples\n", x , y);
+  printf("one of the most iconic numbers in math is %f the PI number\n" , PI);
+  printf("do you know that the speed of light is %f, it can travel to the sun within minutes!\n" , LS);
+  return 0;
+}
